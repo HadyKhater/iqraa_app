8d6f741af7bbc544ce0f452d84a779c361556faa
hady Khater <hadyhanykhater@gmail.com>
2024-06-02 22:26:56 +0300

    edite width and height in custom bottom sheet in research view

--- lib/Widget/custom_bottom_sheet.dart
+++ lib/Widget/custom_bottom_sheet.dart
@@ -11,92 +11,99 @@ class CustomBottomSheet extends StatelessWidget {
   final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: SizedBox(
-        width: double.infinity,
-        height: 462.0.h,
-        child: Directionality(
-          textDirection: TextDirection.rtl,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 35.0.h,
-              ),
-              Center(
-                child: CustomText(
-                    title: 'بحث',
-                    fontSize: 24.0.sp,
-                    fontWeight: FontWeight.w700,
-                    colorText: kSecondColor),
-              ),
-              SizedBox(
-                height: 20.0.h,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24.0.w),
-                child: CustomText(
-                    title: 'التصنيفات',
-                    fontSize: 24.0.sp,
-                    fontWeight: FontWeight.w700,
-                    colorText: kSecondColor),
-              ),
-              SizedBox(
-                height: 24.0.h,
-              ),
-              SizedBox(
-                height: 50.0.h,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 24.0.w),
-                  child: ListView.separated(
-                      scrollDirection: Axis.horizontal,
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) =>
-                          const CustomCategoriesItem(
-                            text: 'الكل',
-                          ),
-                      separatorBuilder: (context, index) => SizedBox(
-                            width: 10.0.w,
-                          ),
-                      itemCount: 5),
+    return SizedBox(
+      width: double.infinity,
+      height: 462.0.h,
+      child: Directionality(
+        textDirection: TextDirection.rtl,
+        child: Container(
+          width: 428.0.w,
+          height: 462.0.h,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 35.0.h,
                 ),
-              ),
-              SizedBox(
-                height: 24.0.h,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24.0.w),
-                child: CustomText(
-                    title: 'تقييم',
-                    fontSize: 24.0.sp,
-                    fontWeight: FontWeight.w700,
-                    colorText: kSecondColor),
-              ),
-              SizedBox(
-                height: 24.0.h,
-              ),
-              SizedBox(
-                height: 50.0.h,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 24.0.w),
-                  child: ListView.separated(
-                      scrollDirection: Axis.horizontal,
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) => const CustomRateItem(),
-                      separatorBuilder: (context, index) => SizedBox(
-                            width: 10.0.w,
-                          ),
-                      itemCount: 5),
+                Center(
+                  child: CustomText(
+                      title: 'بحث',
+                      fontSize: 24.0.sp,
+                      fontWeight: FontWeight.w700,
+                      colorText: kSecondColor),
                 ),
-              ),
-              SizedBox(
-                height: 15.0.h,
-              ),
-              CustomButton(
-                title: 'بحث',
-                onTap: onTap,
-              ),
-            ],
+                SizedBox(
+                  height: 20.0.h,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 24.0.w),
+                  child: CustomText(
+                      title: 'التصنيفات',
+                      fontSize: 24.0.sp,
+                      fontWeight: FontWeight.w700,
+                      colorText: kSecondColor),
+                ),
+                SizedBox(
+                  height: 24.0.h,
+                ),
+                SizedBox(
+                  height: 50.0.h,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 24.0.w),
+                    child: ListView.separated(
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) =>
+                            const CustomCategoriesItem(
+                              text: 'الكل',
+                            ),
+                        separatorBuilder: (context, index) => SizedBox(
+                              width: 10.0.w,
+                            ),
+                        itemCount: 5),
+                  ),
+                ),
+                SizedBox(
+                  height: 24.0.h,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 24.0.w),
+                  child: CustomText(
+                      title: 'تقييم',
+                      fontSize: 24.0.sp,
+                      fontWeight: FontWeight.w700,
+                      colorText: kSecondColor),
+                ),
+                SizedBox(
+                  height: 24.0.h,
+                ),
+                SizedBox(
+                  height: 50.0.h,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 24.0.w),
+                    child: ListView.separated(
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) => const CustomRateItem(),
+                        separatorBuilder: (context, index) => SizedBox(
+                              width: 10.0.w,
+                            ),
+                        itemCount: 5),
+                  ),
+                ),
+                SizedBox(
+                  height: 15.0.h,
+                ),
+                CustomButton(
+                  title: 'بحث',
+                  onTap: onTap,
+                ),
+                SizedBox(
+                  height: 15.0.h,
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/Widget/custom_log_out_show_bottom_sheet.dart
+++ lib/Widget/custom_log_out_show_bottom_sheet.dart
@@ -10,53 +10,57 @@ class CustomLogOutShowBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        CustomText(
-          title: 'تسجيل الخروج',
-          fontSize: 24.0.sp,
-          fontWeight: FontWeight.w700,
-          colorText: const Color(0xffF75555),
-        ),
-        SizedBox(
-          height: 24.0.h,
-        ),
-        Divider(
-          height: 1,
-          indent: 20.0.w,
-          endIndent: 5.0.w,
-          thickness: 0.2,
-        ),
-        SizedBox(
-          height: 24.0.h,
-        ),
-        CustomText(
-          title: 'هل تريد تسجيل الخروج',
-          fontSize: 20.0.sp,
-          fontWeight: FontWeight.w700,
-          colorText: kSecondColor,
-        ),
-        SizedBox(
-          height: 50.0.h,
-        ),
-        Row(
-          children: [
-            CustomButton(
-              title: 'الغاء',
-              width: 140.0.w,
-              height: 58.0.h,
-              color: Colors.white,
-              textcolor: kPrimaryColor,
-            ),
-            CustomButton(
-              title: 'نعم (اخرج)',
-              width: 140.0.w,
-              height: 58.0.h,
-            ),
-          ],
-        )
-      ],
+    return  Container(
+      width: 428.0.w,
+      height: 266.0.h,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CustomText(
+            title: 'تسجيل الخروج',
+            fontSize: 24.0.sp,
+            fontWeight: FontWeight.w700,
+            colorText: const Color(0xffF75555),
+          ),
+          SizedBox(
+            height: 24.0.h,
+          ),
+          Divider(
+            height: 1,
+            indent: 20.0.w,
+            endIndent: 5.0.w,
+            thickness: 0.2,
+          ),
+          SizedBox(
+            height: 24.0.h,
+          ),
+          CustomText(
+            title: 'هل تريد تسجيل الخروج',
+            fontSize: 20.0.sp,
+            fontWeight: FontWeight.w700,
+            colorText: kSecondColor,
+          ),
+          SizedBox(
+            height: 50.0.h,
+          ),
+          Row(
+            children: [
+              CustomButton(
+                title: 'الغاء',
+                width: 140.0.w,
+                height: 58.0.h,
+                color: Colors.white,
+                textcolor: kPrimaryColor,
+              ),
+              CustomButton(
+                title: 'نعم (اخرج)',
+                width: 140.0.w,
+                height: 58.0.h,
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
